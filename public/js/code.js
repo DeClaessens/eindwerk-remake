c92b81250b04e92dff07d1996e14dd5254bd1ef2
Thomas Claessens <thomas.claessens@vrt.be>
2016-08-22 14:46:48 +0200

    finetuned image resizing

--- public/js/code.js
+++ public/js/code.js
@@ -59,27 +59,34 @@ $('document').ready(function(){
 
         }
     });
-
-    $('.concert-list a').each(function(){
-        var el = $(this);
-        var w = el.width();
-
-        el.children('.concert-image-container').css('height', w);
+    resizeImages();
+    $(window).resize(function(){
+        resizeImages()
     });
-
-    $('.matches-container .match').each(function(){
-        var el = $(this);
-        var w = el.width();
-
-        el.children('.match-image').css('height', w);
-    });
-
-    var landingel = $('.concert-landing-card');
-    var landingW = landingel.width();
-
-    landingel.children('.concert-landing-image').css('height', landingW);
 
 
     $(".message-box").scrollTop($(".message-box")[0].scrollHeight);
 
 });
+
+function resizeImages() {
+        $('.concert-list a').each(function(){
+            var el = $(this);
+            var w = el.width();
+
+            el.children('.concert-image-container').css('height', w);
+        });
+
+        $('.matches-container .match').each(function(){
+            var el = $(this);
+            var w = el.width();
+
+            el.children('.match-image').css('height', w);
+        });
+        /*
+        var landingel = $('.concert-landing-card');
+        var landingW = landingel.width();
+
+        landingel.children('.concert-landing-image').css('height', landingW);
+        */
+}
