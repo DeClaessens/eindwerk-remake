3cd5e6c8671f97ff6f39ab1fb917ced31678c2fa
Thomas Claessens <thomas.claessens@vrt.be>
2016-08-25 07:22:23 +0200

    CSS edits

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,29 +18,29 @@
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                            @endif
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                                @endif
+
                             </div>
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
-
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                            @endif
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password">
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                                @endif
+
                             </div>
                         </div>
 
